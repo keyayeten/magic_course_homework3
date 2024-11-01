284603fe20db32a93e3b0056d5e147d65f666751
qapmaria <mdgrishunina@gmail.com>
2024-11-01 12:48:25 +0300

    task2-3

--- task2.py
+++ task2.py
@@ -3,19 +3,11 @@
 # Функция возвращает кортеж из трёх элементов: путь,
 # имя файла, расширение файла.
 
-# Пример 1:
-# file_path1 = "/home/user/documents/report.txt"
-# print(split_file_path(file_path1))
-# ('/home/user/documents', 'report', 'txt')
-
-# Пример 2:
-# file_path2 = "/var/log/system.log"
-# print(split_file_path(file_path2))
-# ('/var/log', 'system', 'log')
-
-
+import os
 def split_file_path(file_path):
-    pass  # вместо pass - напишите свое решение
+    path, extension = os.path.splitext(file_path)
+    dirname, name = os.path.split(path)
+    return (dirname, name, extension)
 
 
 if __name__ == "__main__":
@@ -35,3 +27,4 @@ if __name__ == "__main__":
         split_file_path(file_path2),
         "Верный ответ - ('/var/log', 'system', 'log')"
     )
+    
