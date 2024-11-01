366bd029d274f3e9e2dece5a9858815881777bc0
qapmaria <mdgrishunina@gmail.com>
2024-11-01 12:50:02 +0300

    task5-3

--- task5.py
+++ task5.py
@@ -2,24 +2,12 @@
 # являются ли две строки анаграммами
 # (содержат одинаковые символы в разном порядке).
 
-# Пример 1:
-# str1_1 = "listen"
-# str2_1 = "silent"
-# print(are_anagrams(str1_1, str2_1))  # True
-
-# Пример 2:
-# str1_2 = "apple"
-# str2_2 = "papel"
-# print(are_anagrams(str1_2, str2_2))  # True
-
-# Пример 3:
-# str1_3 = "hello"
-# str2_3 = "world"
-# print(are_anagrams(str1_3, str2_3))  # False
-
-
 def are_anagrams(str_1, str_2):
-    pass  # вместо pass - напишите свое решение
+    str1_list = list(str_1)
+    str1_list.sort()
+    str2_list = list(str_2)
+    str2_list.sort()
+    return (str1_list == str2_list)
 
 
 if __name__ == "__main__":
@@ -53,3 +41,4 @@ if __name__ == "__main__":
         are_anagrams(str1_3, str2_3),
         "Верный ответ - False"
     )
+    
