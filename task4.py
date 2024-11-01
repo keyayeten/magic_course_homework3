6c500937634818f0830227cc2164c5389b2d9d8b
qapmaria <mdgrishunina@gmail.com>
2024-11-01 12:49:32 +0300

    task4-3

--- task4.py
+++ task4.py
@@ -2,21 +2,16 @@
 # сколько раз каждый элемент встречается в списке.
 # Функция должна возвращать словарь с частотой каждого элемента.
 
-# Пример 1:
-# lst1 = [1, 2, 2, 3, 3, 3, 4]
-# print(count_frequencies(lst1))
-# {1: 1, 2: 2, 3: 3, 4: 1}
-
-# Пример 2:
-# lst2 = ['a', 'b', 'a', 'c', 'b', 'b', 'a']
-# print(count_frequencies(lst2))
-# {'a': 3, 'b': 3, 'c': 1}
-
-
 def count_frequencies(lst):
-    pass  # вместо pass - напишите свое решение
-
-
+    frequencies = {}
+    for i in lst:
+        if i in frequencies:
+            frequencies[i] = frequencies[i] + 1
+        else:
+            frequencies[i] = 1
+    return frequencies
+        
+    
 if __name__ == "__main__":
     lst1 = [1, 2, 2, 3, 3, 3, 4]
     lst2 = ['a', 'b', 'a', 'c', 'b', 'b', 'a']
@@ -34,3 +29,4 @@ if __name__ == "__main__":
         count_frequencies(lst2),
         "Верный ответ - {'a': 3, 'b': 3, 'c': 1}"
     )
+    
